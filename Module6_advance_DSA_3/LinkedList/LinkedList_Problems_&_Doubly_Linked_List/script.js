33a32c2e98d716b61729631ca33856d1d62d7fca
kumar aditya <aditya108pathak@gmail.com>
2026-07-16 00:36:04 +0400

    LinkedList: Interaction question solved, added frequntly asked question form linkedList and Update file system

---
+++ Module6_advance_DSA_3/LinkedList/LinkedList_Problems_&_Doubly_Linked_List/script.js
@@ -0,0 +1,66 @@
+const QuestionsLists = [
+    "Given 2 LinkedList which interect at some points, find the interaction point."
+];
+
+listObject('class_topic', QuestionsLists);
+
+class Node{
+    constructor(val){
+        this.element = val;
+        this.next = null;
+    }
+}
+class LinkedListIII{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    creatLLOutOfArray(arr = [1,2,3,4,5,6]){
+        let n = arr.length;
+        this.head = new Node(arr[0]);
+
+        let curr = this.head;
+        for(let i=1; i<n; i++){
+            curr.next = new Node(arr[i]);
+            curr = curr.next;
+            this.size++;
+        }
+        return this.head;
+    }
+
+    // interact 2 LinkedList
+    creatingInteraction(h1, val){
+        let c1 = h1;
+        while(c1){
+            c1 = c1.next;
+            if(c1.element === val) break;
+        }
+        console.log("c1 : ", c1);
+        return c1;
+    }
+
+    findInteractionPoint(){
+        let h1 = this.creatLLOutOfArray([5,3,6,7,2,0,8]);
+        let h2 = this.creatLLOutOfArray([8,4]);
+        h2.next = this.creatingInteraction(h1,7); // Here interaction done;
+        console.log(h1, h2);
+
+        let p1 = h1;
+        let p2 = h2;
+        while(p1 && p2){
+            p1 = p1.next;
+            p2 = p2.next;
+
+            if(!p1) p1 = h2;
+            if(!p2) p2 = h1;
+
+            if(p1.element === p2.element) return {ans: `We have pointer ${p1.element}`, p1};
+        }
+        return -1;
+    }
+}
+
+const llIII = new LinkedListIII()
+
+
