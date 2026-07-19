1ed151d7f60bdc729dc87ec9592ab35dbe6dc395
kumar aditya <aditya108pathak@gmail.com>
2026-07-19 17:49:55 +0400

    LinkedList3: Deep copy of given LinkedList

--- Module6_advance_DSA_3/LinkedList/LinkedList3_Problems_&_Doubly_Linked_List/script.js
+++ Module6_advance_DSA_3/LinkedList/LinkedList3_Problems_&_Doubly_Linked_List/script.js
@@ -12,6 +12,15 @@ class Node{
         this.random = null;
     }
 }
+
+class NodeWithRandom{
+    constructor(val){
+        this.element = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
 class LinkedListIII{
     constructor(){
         this.head = null;
@@ -101,13 +110,122 @@ class LinkedListIII{
 
     // Deep copy the LinkedList which has random pointer
     deepCopyWithRandomPointer(){
+        let ll = this.createLinkedListWithRandomPointer([10, 20, 30, 40]);
+
+        /**
+         * The main motive is to do a deep copy from a given LinkedList with Random pointer.
+         * To do so, 1st create new node(next=null and random=null) from the given linkedlist next pointers.
+         * Side by side save the given LinkedList node pointers(next and random) in the HashMap.
+         * 
+         * Once done the above, loop again from begining and now get the random from the hashmap and assign it to 
+         * newly created LinkedList's random pointer;
+         * 
+         * That's all
+         */
         const bf_hasing = () => {
+            let map = new Map();
+            let h1 = ll;
+            let h2 = new NodeWithRandom(-1);
+            let tail = h2;
+            while(h1){
+                tail.next = new NodeWithRandom(h1.element);
+                tail = tail.next;
+                map.set(h1.element, h1);
+                h1 = h1.next;
+            }
+            h2 = h2.next;
+            let newLL = h2;
+            // console.log("h2: ", h2, h1);
+            
+            h1 = ll;
+            while(h1){
+                let h1_random = map.get(h1.element);
+                newLL.random = h1_random.random;
+                newLL = newLL.next;
+                h1 = h1.next;
+            }
+
+            console.log("h1 : ", ll);
+            console.log("final bf h2 :: ", h2);
+        }
+
+        /**
+         * This is similar as above, 
+         * Just I have replace HashMap and directly get data from given
+         * LinkedList in 2nd loop to set Random values
+         */
+        const optimal1 = () => {
+            let old = ll;
+            let h2 = new NodeWithRandom(-1);
+            let newLL = h2;
+            while(old){
+                let ele = old.element;
+                newLL.next = new NodeWithRandom(ele);
+                newLL = newLL.next;
+
+                old = old.next;
+            }
+
+            old = ll;
+            h2 = h2.next;
+            newLL = h2
+            while(old){
+                let randomVal = old.random;
+                newLL.random = randomVal;
+
+                newLL = newLL.next;
+                old = old.next;
+            }
+            console.log("Final Optimal-1 h2 : ", h2);
+        }
+
+        /**
+         * In this we are just coping the prev/current values and adding it as the extra pointer as the next address
+         * later we are destructuring old LinkedLint and newly created LinkedList(added in as next pointer) 
+         * 
+         * TC: O(n) = O(n);
+         * SC: O(1) = O(1)
+         */
+        const optimal2 = () => {
+            let old = ll;
+            let h1 = old;
+            
+            //Allocating Next pointer here
+            while(h1){
+                let x= new NodeWithRandom(h1.element);
+                x.next = h1.next;
+                h1.next = x;
+                h1 = x.next;
+            }
+
+            h1 = old;
+            //Allocating Random pointer here
+            while(h1){
+                h1.next.random = h1.random;
+                h1 = h1.next.next;
+            }
+
+            h1 = old;
+            let h2 = new NodeWithRandom(-1);
+            let newLL = h2;
+            // newLL = h1.next;
+            //Now depart new and old LinkedList
+            while(h1){
+                newLL.next = h1.next;
+                newLL = newLL.next;
+
+                h1.next = newLL.next;
+                h1 = h1.next;
+            }
+            h2 = h2.next;
+
+
+            console.log("final optimal2 h1: ", old);
+            console.log("final optimal2 new: ", h2);
             
         }
 
-        const optimal = () => {
-            
-        }
+        return {bf_hasing : bf_hasing(), optimal1:optimal1(), optimal2:optimal2()};
     }
 }
 
