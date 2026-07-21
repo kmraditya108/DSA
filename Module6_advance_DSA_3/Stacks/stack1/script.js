dce4edfbd635d6014e5fcac13f5d88e6c9d24cce
kumar aditya <aditya108pathak@gmail.com>
2026-07-21 17:18:36 +0400

    Stack - Valid Parenthesis function implemented

--- Module6_advance_DSA_3/Stacks/stack1/script.js
+++ Module6_advance_DSA_3/Stacks/stack1/script.js
@@ -1,6 +1,8 @@
 const QuestionsLists = [
     "Implement stack using array",
-    "Implement stack using LinkedList"
+    "Implement stack using LinkedList",
+    "Valid Parentheses : Check if a given sequence of Parentheses is valid or not. |--- OR---| Given a string containing (), {}, [], determine if the input string is valid.",
+
 ];
 
 listObject('class_topic', QuestionsLists);
@@ -15,13 +17,13 @@ listObject('class_topic', QuestionsLists);
  */
 
 
-class StackFromArray{
-    constructor(){
+class StackFromArray {
+    constructor() {
         this.stackArr = [];
         this.size = 0;
     }
 
-    push(val){
+    push(val) {
         this.stackArr[this.size] = val;
         this.size++;
 
@@ -29,23 +31,23 @@ class StackFromArray{
     }
 
     // Remove the recent added data/value/node
-    pop(){
-        if(!this.size) throw new Error("Null pointer exception!! \n Stack is already empty..");
-        this.stackArr.length = (this.size-1)
+    pop() {
+        if (!this.size) throw new Error("Null pointer exception!! \n Stack is already empty..");
+        this.stackArr.length = (this.size - 1)
         this.size--;
     }
 
-    peek(){
-        if(!this.size) throw new Error("Null pointer exception!!\nNo stacks available.")
+    peek() {
+        if (!this.size) throw new Error("Null pointer exception!!\nNo stacks available.")
 
-        return this.stackArr[this.size-1];
+        return this.stackArr[this.size - 1];
     }
 
-    isEmpty(){
-        return this.size===0;
+    isEmpty() {
+        return this.size === 0;
     }
 
-    clear(){
+    clear() {
         this.stackArr.length = 0;
         this.size = 0;
 
@@ -56,31 +58,37 @@ class StackFromArray{
 const stack_from_arr = new StackFromArray();
 
 
-function fillArrayStack(arr=[10, 12,14,15,20,34]){
-    for(let i of arr)
+function fillArrayStack(arr = [10, 12, 14, 15, 20, 34]) {
+    for (let i of arr)
         stack_from_arr.push(i);
 
     return stack_from_arr;
 }
 // =========================================Stack implementation using LinkedList================================
 
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.element = val;
         this.next = null;
     }
 }
 
-class StackFromLinkedList{
-    constructor(stack_length){
+class StackFromLinkedList {
+    constructor(stack_length) {
         this.head = null;
         this.size = 0;
         this.capacity = stack_length;
+
+        // this.push = this.push.bind(this);
+        // this.pop = this.pop.bind(this);
+        // this.peek = this.peek.bind(this);
+        // this.isEmpty = this.isEmpty.bind(this);
+        // this.clear = this.clear.bind(this);
     }
 
-    push(val){
-        if(this.size>=this.capacity) throw new Error("Error Stack OutOfBound!!\nStack already reach to it's maximum length...");
-        
+    push = (val) => {
+        if (this.size >= this.capacity) throw new Error("Error Stack OutOfBound!!\nStack already reach to it's maximum length...");
+
         let x = new Node(val);
         x.next = this.head;
         this.head = x;
@@ -90,14 +98,14 @@ class StackFromLinkedList{
         return this.head;
     }
 
-    peek(){
-        if(!this.size) throw new Error('Null pointer exception!!\nStack is already empty..');
+    peek = () => {
+        if (!this.size) throw new Error('Null pointer exception!!\nStack is already empty..');
 
         return this.head.element;
     }
 
-    pop(){
-        if(!this.size) throw new Error('Null pointer exception!!\nStack is already empty..');
+    pop = () => {
+        if (!this.size) throw new Error('Null pointer exception!!\nStack is already empty..');
 
         this.head = this.head.next;
         this.size--;
@@ -105,22 +113,81 @@ class StackFromLinkedList{
         return this.head;
     }
 
-    isEmpty(){
-        return this.size===0;
+    isEmpty = () => {
+        return this.size === 0;
     }
 
-    clear(){
+    clear = () => {
         this.head = null;
         this.size = 0;
         return this.head;
     }
+
+    // to get dynamic head in destructuring call in another function call
+    getHead = () => this.head;
+
+    // to get dynamic size in destructuring call in another function call
+    getSize = () => this.size;
 }
 
-const LinkedListStack = new StackFromLinkedList(5);
+const LinkedListStack = new StackFromLinkedList(10);
 
-function fillLinkedListStack(arr=[10,12,14,15,20]){
-    for(let i of arr)
-        LinkedListStack.push(i); 
+function fillLinkedListStack(arr = [10, 12, 14, 15, 20]) {
+    for (let i of arr)
+        LinkedListStack.push(i);
 
     return LinkedListStack;
+}
+
+// ==================================== Other Stack related question =====================================
+
+
+function isValidParenthesis(str = '({})[]') {
+
+    const { push, peek, pop, isEmpty, getSize, getHead } = LinkedListStack;// new StackFromLinkedList(10); // -----------> Destructuring method of ES6
+
+    // if string is valid parenthesis, then it should be in pair means multiple of 2, hence if it's not % of 2 then false;
+    if (str.length % 2 !== 0) return false;
+
+    const approach1 = () => {
+        for (let v of str) {
+            if ((v === '(') || (v === '{') || (v === '[')) {
+                push(v);
+                console.log("head : ", getHead());
+                console.log("head-1 : ", LinkedListStack.head);
+
+            } else {
+                const parenthesis = peek();
+                if ((parenthesis === '(') && (v !== ')')) return false;
+                if ((parenthesis === '{') && (v !== '}')) return false;
+                if ((parenthesis === '[') && (v !== ']')) return false;
+
+                pop();
+            }
+        }
+        return { size: getSize(), head: getHead(), isValidParenthesis: true };
+    }
+
+    const approach2 = () => {
+        const pair_mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for (let v of str) {
+            if (v in pair_mapping) {
+                const hVal = peek();
+
+                if (isEmpty() || hVal !== pair_mapping[v]) return false;
+                pop();
+            } else {
+                push(v);
+            }
+        }
+
+        return { size: getSize(), head: getHead() };
+    }
+
+    return { approach1: approach1(), approach2: approach2() };
 }
