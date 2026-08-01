859f78deee92c8ade4beba5ef97fb6a5b70430c0
kumar aditya <aditya108pathak@gmail.com>
2026-08-01 23:27:05 +0530

    Queue: Queue integration using Slack (#31)

--- Module6_advance_DSA_3/Queue/script.js
+++ Module6_advance_DSA_3/Queue/script.js
@@ -1,5 +1,6 @@
 const QuestionsLists = [
-    "Queue implementation using LinkedList."
+    "Queue implementation using LinkedList.",
+    "Queue implementation using Stack."
 ];
 
 listObject('class_topic', QuestionsLists);
@@ -12,6 +13,9 @@ class Node{
     }
 }
 
+/**
+ * Que: Queue implementation using LinkedList.
+ */
 class QueueUsingLL{
     constructor(capacity=5){
         this.capacity = capacity;
@@ -60,7 +64,7 @@ class QueueUsingLL{
         this.head = this.head.next;
         this.size -= 1;
         if(this.head===null) this.tail=null;
-        
+
         return this.head;
     }
 
@@ -96,3 +100,109 @@ class QueueUsingLL{
 }
 
 const queueUsingLL = new QueueUsingLL();
+
+/**
+ * Que: Queue implementation using Stack.
+ * To implement this, we have to have 2 stack s1, s2.
+ * Enqueue : All enqueue operations are done in s1 stack
+ * Dequeue : All dequeue operation will be happening from s2 stack.
+ * isEmpty : We have to check both the stacks.
+ * Size : To get size we have to iterate through both stacks and count the data length.
+ * 
+ * Enqueue in s1, 
+ * For dequeu() calls, 
+ *  1. Check s2, if data availabe remove the top most data
+ *  2. if data not available, get all the data from s1 and fill it to s2 and then remove data in LIFO order
+ */
+class Stack{
+    constructor(capacity=5){
+        this.head = null;
+        this.size = 0;
+        this.capacity = capacity
+    }
+
+    push(val){
+        let x = new Node(val);
+        if(!this.head){
+            this.head = x;
+        }else{
+            x.next = this.head;
+            this.head = x;
+        }
+        this.size+=1;
+        return this.head;
+    }
+    pop(){
+        if(!this.size) throw new Error('Error! Stack is empty.')
+        this.head = this.head.next;
+        this.size-=1;
+        return this.head;
+    }
+    peek(){
+        if(!this.size) throw new Error('Error! Stack is empty.');
+        return this.head.element;
+    }
+    isEmpty(){
+        return this.size === 0;
+    }
+}
+class QueueUsingStack{
+    constructor(){
+        this.s1 = new Stack(7);
+        this.s2 = new Stack(7);
+    }
+
+    enqueue(val){
+        this.s1.push(val);
+        return this.s1.head;
+    }
+
+    dequeue(){
+        // if(this.s2.size===0) throw new Error('Error!! Queue already empty.');
+        let dequedVal = undefined;
+
+        // 1st check, if s2 is empty then 1st pull data from s1 and then deueue
+        if(this.s2.size===0){
+            if(this.s1.size===0) throw new Error('Error!! Queue already empty.');
+
+            let curr = this.s1.head;
+            while(curr){
+                let val = this.s1.peek();
+                this.s2.push(val);
+                curr = this.s1.pop();
+            }
+            dequedVal = this.s2.peek();
+            this.s2.pop();
+        }else{
+            dequedVal = this.s2.peek();
+            this.s2.pop();
+        }
+        return {s1:this.s1, s2:this.s2, dequedVal}
+    }
+
+    front(){
+        if(this.s2.isEmpty()){
+            if(this.s1.isEmpty()) throw new Error("Error!! Queue is already empty");
+
+            while(!this.s1.isEmpty()){
+                let top = this.s1.peek();
+                this.s2.push(top);
+                this.s1.pop();
+            }
+            return this.s2.peek();
+        }else{
+            return this.s2.peek(); 
+        }
+    }
+
+    getSize(){
+        return (parseInt(this.s2.size)+parseInt(this.s1.size))
+    }
+
+    isEmpty(){
+        return (parseInt(this.s2.size)+parseInt(this.s1.size)) === 0
+    }
+}
+
+const stck = new Stack();
+const queueUsingStck = new QueueUsingStack();   
