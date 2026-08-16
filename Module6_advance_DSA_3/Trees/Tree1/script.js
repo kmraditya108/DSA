b62ebcd9f893c4f888675ad6b56ab104818baf08
kumar aditya <aditya108pathak@gmail.com>
2026-08-16 17:51:48 +0530

    Tree: tree creation, and traversal order method integrated (#35)
    * Tree: tree creation, and traversal order method integrated
    
    * Tree: clear index.html removed unwanted files

--- Module6_advance_DSA_3/Trees/Tree1/script.js
+++ Module6_advance_DSA_3/Trees/Tree1/script.js
@@ -6,29 +6,82 @@ const QuestionsLists = [
 
 listObject('class_topic', QuestionsLists);
 class Node{
-    constructor(root, left, right){
+    constructor(root){
         this.root = root;
         this.left = null;// left;
         this.right = null;//right;
     }
 }
 class Tree1{
-    insert(){
-        let arr = [5,2,9,8,7,0]
-        let n = arr.length;
-        
-
-        for(let v of arr){
-            let x = new Node(v);
-            
-        }
+    constructor(){
+        this.treeRoot = null;
     }
-    preOrder(arr = [5,2,9,8,7,0]){
+    
+    createTree(arr = [5,2,9,8,7,0]){
         let n = arr.length;
+        const tree=(arr, i)=>{
+            if((i>=arr.length) || (arr[i] === null)|| (arr[i] === undefined)) return;
+            
+            let root = new Node(arr[i]);
+            if(i==0) this.treeRoot = root;
 
-        const recursiveCall = () => {
-
+            root.left = tree(arr, 2*i+1);
+            root.right = tree(arr, 2*i+2);
+            
+            return root;
         }
+        return tree(arr, 0);
+    }
+  
+    /**
+     * arr = [5,2,9,8,7,0]
+     * Pre-order: t = {n:2, l:5, r:9, | n:7, l:8, r:0}
+     */
+    preOrder(arr = [5,2,9,8,7,0]){
+        this.createTree(arr);
+
+        let result = '';
+        const preOrderTraversal=(root)=>{
+            if(!root) return;
+            result = root.root;
+            console.log(root.root);
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+        
+        preOrderTraversal(this.treeRoot);
+        // return result;
+    }
+
+    postOrder(arr=[1, 2, 3, 4, 5, null, 6]){
+        this.createTree(arr);
+
+        // Left -> right -> root
+        const postOrderTraversal = (root) => {
+            if(!root) return;
+
+            postOrderTraversal(root.left);
+            postOrderTraversal(root.right);
+            console.log(root.root);
+        }
+
+        postOrderTraversal(this.treeRoot);
+    }
+
+    /**
+     * Left -> Root -> right
+     */
+    inOrder(arr=[1, 2, 3, 4, 5, null, 6]){
+        this.createTree(arr);
+        const inOrderTraversal = (root) => {
+            if(!root) return;
+
+            inOrderTraversal(root.left);
+            console.log(root.root);
+            inOrderTraversal(root.right);
+        }
+
+        inOrderTraversal(this.treeRoot)
     }
 }
 
